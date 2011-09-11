b0d9864e7f06c133707fd0701c3badfd142d3caa
opensas <opensas@gmail.com>
2011-09-11 18:07:29 -0300

    crud fully functional

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -4,6 +4,9 @@ import java.util.List;
 
 import lib.jobs.BootstrapJob;
 import models.Event;
+import models.EventType;
+import play.data.validation.Valid;
+import play.mvc.Before;
 import play.mvc.Controller;
 
 public class Application extends Controller {
@@ -19,9 +22,34 @@ public class Application extends Controller {
     	list();
     }
     
+    public static void form(Long id) {
+    	final Event event;
+    	if (id==null) {
+    		event = new Event();
+    	} else {
+    		event = Event.findById(id);
+    	}
+    	render(event);
+    }
+
+    public static void save(@Valid Event event) {
+    	if (validation.hasErrors()) {
+    		render("@form", event);
+    	}
+    	event.save();
+    	flash.success("event successfully saved!");
+    	list();
+    }
+    
     public static void loadFromYamlFile() {
     	new BootstrapJob().doJob();
     	list();
     }
+    
+	@SuppressWarnings("unused")
+	@Before
+	private static void loadEventTypes() {
+		renderArgs.put("types", EventType.find("order by name").fetch());
+	}
     
 }
