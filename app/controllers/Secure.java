6dbc0cbbb3ec676ebd570d63e00f334095e223aa
opensas <opensas@gmail.com>
2011-09-14 03:03:20 -0300

    authentication finished

--- app/controllers/Secure.java
+++ app/controllers/Secure.java
@@ -4,6 +4,7 @@ import models.User;
 import play.Logger;
 import play.Play;
 import play.libs.OAuth;
+import play.libs.OAuth2;
 import play.libs.WS;
 import play.mvc.Before;
 import play.mvc.Controller;
@@ -13,6 +14,7 @@ import com.google.gson.JsonObject;
 public class Secure extends Controller {
 
 	private final static String USER_COOKIE = "user";
+	
 	private final static OAuth.ServiceInfo TWITTER = new OAuth.ServiceInfo(
 		Play.configuration.getProperty("twitter.requestTokenURL"),
 		Play.configuration.getProperty("twitter.accessTokenURL"),
@@ -21,7 +23,13 @@ public class Secure extends Controller {
 		Play.configuration.getProperty("twitter.consumerSecret")
 	);
 
-	
+	public static OAuth2 FACEBOOK = new OAuth2(
+	    Play.configuration.getProperty("facebook.authorizationURL"),
+	    Play.configuration.getProperty("facebook.accessTokenURL"),
+	    Play.configuration.getProperty("facebook.clientid"),
+	    Play.configuration.getProperty("facebook.secret")
+	);
+
 	@Before(only={"Application.form", "Application.save", "Application.delete"})
 	static void checkAccess() {
 		final String userId = session.get(USER_COOKIE);
@@ -108,6 +116,35 @@ public class Secure extends Controller {
 			Application.list();
 		}
 	}
+
+	public static void oauthFacebook() throws Throwable
+    {
+        if ( params.get("error") != null ) {
+            // El usuario no autorizo el acceso
+            // error_reason y error_description traen mas info
+            login();
+        }
+        final String authUrl = play.mvc.Router.getFullUrl("Secure.oauthFacebook");
+
+        if (OAuth2.isCodeResponse()) {
+            // El usuario autorizo el acceso desde Facebook
+            OAuth2.Response response = FACEBOOK.retrieveAccessToken(authUrl);
+            User user = User.find("accessToken = ?", response.accessToken).first();
+            if ( user == null ) {
+                user = new User();
+                user.accessToken = response.accessToken;
+            }
+            JsonObject me = WS.url("https://graph.facebook.com/me?fields=name,picture&access_token=%s", WS.encode(user.accessToken)).get().getJson().getAsJsonObject();
+            user.name = me.get("name").getAsString();
+            user.avatarUrl = me.get("picture").getAsString();
+            user.save();
+            session.put(USER_COOKIE, user.id);
+
+            Application.list();
+        }
+        FACEBOOK.retrieveVerificationCode(authUrl);
+    }
+	
 	
 	public static void login() {
 		render();
