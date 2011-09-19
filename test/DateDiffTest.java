20976b5eab58c6aefa21adadb5b98765b065ba41
opensas <opensas@gmail.com>
2011-09-18 22:20:42 -0300

    added languages.en

---
+++ test/DateDiffTest.java
@@ -0,0 +1,13 @@
+import org.junit.*;
+import java.util.*;
+import play.test.*;
+import models.*;
+
+public class DateDiffTest extends UnitTest {
+
+    @Test
+    public void aVeryImportantThingToTest() {
+        assertEquals(2, 1 + 1);
+    }
+
+}
