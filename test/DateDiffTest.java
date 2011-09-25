0a72cb0aed304085acd0b9d72d10da91e195990f
opensas <opensas@gmail.com>
2011-09-25 19:26:44 -0300

    fail4

--- test/DateDiffTest.java
+++ test/DateDiffTest.java
@@ -48,7 +48,7 @@ public class DateDiffTest extends UnitTest {
     			new DateTime(2012, 2, 2, 11, 31, 11, 0).toDate()
     	));
 
-    	assertEquals("will fail again", DateHelper.dateDiff(
+    	assertEquals("will fail 2", DateHelper.dateDiff(
     			new DateTime(2011, 1, 1, 10, 30, 0, 0).toDate(), 
     			new DateTime(2011, 1, 1, 10, 30, 1, 0).toDate()
     	));
