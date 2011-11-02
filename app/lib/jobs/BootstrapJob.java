ff03ceca8e33a13f0e8619abb1f92bb4f0f6daab
opensas <opensas@gmail.com>
2011-11-02 10:49:11 -0300

    added changes, fixed jugar logo position

--- app/lib/jobs/BootstrapJob.java
+++ app/lib/jobs/BootstrapJob.java
@@ -1,5 +1,9 @@
 package lib.jobs;
 
+import play.i18n.Lang;
+import models.Event;
+import models.EventType;
+import play.Logger;
 import play.jobs.Job;
 import play.jobs.OnApplicationStart;
 import play.test.Fixtures;
@@ -10,7 +14,9 @@ public class BootstrapJob extends Job {
 	@Override
 	public void doJob() {
 		Fixtures.deleteAllModels();
-		Fixtures.loadModels("data.yml");
+		Fixtures.loadModels("data-" + Lang.get() + ".yml");
+
+		Logger.info("ran BootstrapJob, %s events loaded, %s types loaded", Event.count(), EventType.count());
 	}
 	
 }
